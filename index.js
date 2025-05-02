0309d2d6f0387844fa5301d1553f5b8f90659446
hassannyx <xwe76ox@gmail.com>
2025-05-02 18:06:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { OpenAI } = require('openai');
 const fs = require('fs');
-require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
